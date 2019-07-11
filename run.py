9a6e6784a6a7627843115afd921d2a748e29e903
Vissurra <461968025@qq.com>
2019-07-11 15:04:47 +0800

    load item sets data from file

--- run.py
+++ run.py
@@ -1,26 +1,15 @@
 from src.fp_growth import FpGrowth
 
 if __name__ == '__main__':
-    item_sets = [
-        [1, 2, 3, 4, 5],
-        [5, 4, 3, 2, 1],
-        [1, 2, 3, 4, 6],
-        [1, 2, 3, 5],
-        [1, 2, 3],
-        [2],
-        [2, 7],
-        [2, 7],
-        [2, 7],
-        [2, 8],
-        [2, 3],
-        [2, 3],
-        [2, 3],
-        [2, 3],
-        [2, 1],
-    ]
+    item_sets = []
+    with open('data/item_sets', mode='r', encoding='utf-8') as file:
+        for line in file:
+            item_sets.append(line.strip().split(' '))
 
     fp = FpGrowth(min_support=3)
     fp.build(item_sets=item_sets)
+
     frequent_item_sets = fp.find_frequency_item_sets()
+    print('-' * 20, 'frequent item sets', '-' * 20)
     for item_set in frequent_item_sets:
         print(item_set)
